20a3d50ab03fcab87e75ef87c73f2aee6bc72652
Gabriel Angelo <gabriel_teodoro123@hotmail.com>
2026-09-15 18:24:04 -0300

    feat: adiciona estrutura inicial de ativos

--- ativos.py
+++ ativos.py
@@ -1,11 +1,21 @@
 #Sistema de inventario de Segurança
 from enum import Enum
 
+#Definição da classe TipoAtivo que herda de Enum para representar os tipos de ativos disponíveis no sistema de inventário de segurança. Cada tipo de ativo é associado a um valor inteiro único.
 class TipoAtivo(Enum):
     NOTEBOOK = 1
     SERVIDOR = 2
     ROTEADOR = 3
     ESTACAO_TRABALHO = 4
 
+ativos = {}
 
-print(TipoAtivo.NOTEBOOK.value)
+ativos[101] = {
+    "nome": "Notebook Dell XPS 13",
+    "tipo": TipoAtivo.NOTEBOOK,
+    "setor": "TI",
+    "responsavel": "João Silva",
+    "vulnerabilidades":[]
+}
+
+print(ativos)
